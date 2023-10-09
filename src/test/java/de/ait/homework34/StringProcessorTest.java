326dd46b321c91ca027601a5db16ba8fa7df87e5
MykolaB77 <mykolabychenko77@gmail.com>
2023-10-09 20:47:15 +0200

    34:Project was created, add pom.xml and Class for test

--- src/test/java/de/ait/homework34/StringProcessorTest.java
+++ src/test/java/de/ait/homework34/StringProcessorTest.java
@@ -1,5 +1,6 @@
 package de.ait.homework34;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,32 +8,35 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringProcessorTest {
+    private StringProcessor processor;
+
+    @BeforeEach
+    public void setUp() {
+        processor = new StringProcessor();
+    }
 
     @Test
     public void testConcatenate() {
-        StringProcessor processor = new StringProcessor();
-        String result = processor.concatenate("Hi ", "Mykola");
-        assertEquals("Hi Mykola", result);
-
+        assertEquals("Hi Mykola",processor.concatenate("Hi ","Mykola"));
     }
 
     @Test
     public void testGetLength() {
-        StringProcessor processor = new StringProcessor();
-        int length = processor.getLength("Bremen ");
-        assertEquals(7, length);
+        //  StringProcessor processor = new StringProcessor();
+        //  int length = processor.getLength("Bremen ");
+        assertEquals(7,processor.getLength("Bremen "));
     }
 
     @Test
     public void testReverse() {
-        StringProcessor processor = new StringProcessor();
-        String reversed = processor.reverse("Unit ");
-        assertEquals(" tinU", reversed);
+       // StringProcessor processor = new StringProcessor();
+       // String reversed = processor.reverse("Unit ");
+        assertEquals(" tinU",processor.reverse("Unit "));
     }
 
     @Test
     public void testIsPalindrome() {
-        StringProcessor processor = new StringProcessor();
+       // StringProcessor processor = new StringProcessor();
         assertTrue(processor.isPalindrome("racecar"));
         assertFalse(processor.isPalindrome("hello"));
 
